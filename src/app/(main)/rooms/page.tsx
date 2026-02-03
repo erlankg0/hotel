705025ca1b08d1cb11f9c644bcd283504ea32a38
erlankg0 <era.ab.02@gmail.com>
2026-02-04 00:03:43 +0300

    start to work rooms page

--- src/app/(main)/rooms/page.tsx
+++ src/app/(main)/rooms/page.tsx
@@ -1,14 +1,47 @@
+import Image from 'next/image';
+
 import { cn } from '@/shared/lib/utils';
 import { BreadcrumbsUI } from '@/widget/breadcrumbs';
-import { PriceRequestFullSize } from '@/widget/price-request-form';
+import { SwiperUI } from '@/widget/swiper';
 
 import styles from './page.module.scss';
 
 export default function Page() {
   return (
     <section className={cn(styles.section)}>
-      <BreadcrumbsUI />
-      <PriceRequestFullSize />
+      <div className="container">
+        <BreadcrumbsUI />
+      </div>
+
+      <SwiperUI
+        delay={5000}
+        slides={[
+          <div key="1" className="relative w-full h-150">
+            <Image
+              src="/images/parallax/hotel/sea.png"
+              alt="Sea view"
+              fill
+              className="object-cover"
+              sizes="100vw"
+              loading={'lazy'}
+            />
+          </div>,
+
+          <div key="2" className="relative w-full h-150">
+            <Image
+              src="/images/parallax/hotel/hotel.png"
+              alt="Hotel view"
+              fill
+              className="object-cover"
+              sizes="100vw"
+              loading={'lazy'}
+            />
+          </div>,
+        ]}
+      />
+      <div className={'container'}>
+        cards
+      </div>
     </section>
   );
 }

4ba8bb975202bf244bc05d9234efc24248094180
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2026-02-20 15:08:18 +0300

    Gallery

--- src/app/(main)/rooms/[id]/page.tsx
+++ src/app/(main)/rooms/[id]/page.tsx
@@ -3,11 +3,12 @@ import { Gallery } from '@/widget/gallery';
 import styles from './page.module.scss';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const id = await params;
+  const { id } = await params;
   return (
     <section className={styles.section}>
-      Room Detail {id.id}
       <Gallery />
+      <h2>Стандартный номер в виллах {id}</h2>
+      <ul></ul>
     </section>
   );
 }
--- src/widget/gallery/ui/gallery.tsx
+++ src/widget/gallery/ui/gallery.tsx
@@ -1,22 +1,25 @@
+import { Equal, Share2 } from 'lucide-react';
+
 import { Button } from '@/shared/ui/button';
 import {
   Drawer,
   DrawerTrigger,
-  DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerHeader,
   DrawerTitle,
-  DrawerFooter,
+  DrawerClose,
 } from '@/shared/ui/drawer';
 import { ImageUI } from '@/shared/ui/image';
+import { ScrollArea } from '@/shared/ui/scroll-area';
+import { Separator } from '@/shared/ui/separator';
 
 import styles from './styles.module.scss';
 
+
 export function Gallery() {
   return (
-    <section>
-      <article className={styles.gallery}>
+    <section className={styles.gallery}>
+      <article className={styles.gallery__content}>
         <ImageUI
           alt={''}
           src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
@@ -52,40 +55,91 @@ export function Gallery() {
         </div>
       </article>
       <Drawer direction={'bottom'}>
-        <DrawerTrigger asChild>
-          <Button>Посмотреть</Button>
-        </DrawerTrigger>
+        <div className={styles.gallery__controls}>
+          <Button className={styles.gallery__button}>
+            <Share2 />
+            <p>Поделиться</p>
+          </Button>
+          <DrawerTrigger asChild>
+            <Button className={styles.gallery__button}>
+              <Equal />
+              <p>Посмотреть</p>
+            </Button>
+          </DrawerTrigger>
+        </div>
         <DrawerContent className="h-dvh">
-          <DrawerHeader>
-            <DrawerTitle>Move Goal</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          <DrawerHeader className="flex">
+            <DrawerClose asChild>
+              <Button variant="ghost" size="icon">
+                ✕
+              </Button>
+            </DrawerClose>
+            <DrawerTitle>Стандартный номер в виллах</DrawerTitle>
           </DrawerHeader>
 
-          <div className="no-scrollbar overflow-y-auto px-4">
-            {Array.from({ length: 10 }).map((_, index) => (
-              <p
-                key={index}
-                className="style-lyra:mb-2 style-lyra:leading-relaxed mb-4 leading-normal"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            ))}
-          </div>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
+          <ScrollArea className="no-scrollbar overflow-y-auto px-4 flex-1">
+            <div className="p-4">
+              <h4 className="mb-4 text-sm leading-none font-medium">Фотографии</h4>
+              <div className={styles.gallery_list_image}>
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+              </div>
+            </div>
+            <Separator />
+            <div className="p-4">
+              <h4 className="mb-4 text-sm leading-none font-medium">Контент</h4>
+              <div className={styles.gallery_list_image}>
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+                <ImageUI
+                  alt={''}
+                  src={'https://cdn.utopiahotels.com/assets/images/pages/310120222100-lg.webp'}
+                  size={'calc(100% - 2px)'}
+                  aspectRatio={'2 / 1'}
+                />
+              </div>
+            </div>
+          </ScrollArea>
         </DrawerContent>
       </Drawer>
-
     </section>
   );
 }

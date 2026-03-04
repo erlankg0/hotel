876f97e75dbc42b80c66618f849bda37fa850057
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2026-03-04 15:57:29 +0300

    Update page.tsx

--- src/app/(main)/aquapark/page.tsx
+++ src/app/(main)/aquapark/page.tsx
@@ -1,5 +1,15 @@
+import { Hero } from '@/widget/hero';
+
 export default async function Page(){
   return (
-    <section>Aquapark</section>
+    <section>
+      <Hero
+        preTitle={'Аквапарк'}
+        subtitle={'Веселье и умиротворение вместе в сердце природы\n.'}
+        title={['Горки', 'Рафтинг', 'Laguna Bar', 'Детский аквапарк', 'Бассейн с водопадом', 'Снэк Бар', 'Бассейн', 'Бар', 'Зоо парк']}
+        video={'https://cdn.utopiahotels.com/assets/videos/covers/uw-aqua.mp4'}
+        poster={'https://cdn.utopiahotels.com/assets/images/pages/001kaydirakmodal-lg.webp'}
+      />
+    </section>
   )
 }

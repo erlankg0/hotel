c7806ff4fa965be8f52068df16f245c5746ad6ee
erlankg0 <era.ab.02@gmail.com>
2026-03-23 22:59:16 +0300

    gallery

--- src/entities/room/ui/room-price/ui/room-price.tsx
+++ src/entities/room/ui/room-price/ui/room-price.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { ChevronLeft, ChevronRight, MartiniIcon, Tv, Wifi } from 'lucide-react';
+import { useCallback } from 'react';
+
+import { Air, Balcony, Cosmetics, Minibar, Safe, Tree } from '@/shared/icons';
+import { Button } from '@/shared/ui/button';
 import { ImageUI } from '@/shared/ui/image';
 import { Separator } from '@/shared/ui/separator';
-import { SwiperUI } from '@/widget/swiper';
+import { SwiperUI, useSwiperNav } from '@/widget/swiper';
 
 import styles from './styles.module.scss';
+
+import type { Swiper as SwiperType } from 'swiper';
 
 const IMAGES = [
   'hotel.webp',
@@ -14,17 +23,80 @@ const IMAGES = [
 
 const gallery = IMAGES.map((image) => `/images/${image}`);
 
+const FEATURES = [
+  { label: 'Море или Лес', Icon: Tree },
+  { label: 'Балкон', Icon: Balcony },
+  { label: 'Кондиционер', Icon: Air },
+  { label: 'Wi-Fi', Icon: Wifi },
+  { label: 'Мини-бар', Icon: Minibar },
+  { label: 'Косметика', Icon: Cosmetics },
+  { label: '32-дюймовый ТВ', Icon: Tv },
+  { label: 'Сейф', Icon: Safe },
+];
+
 
 export function RoomPrice() {
+  const {
+    onSwiper,
+    onSlideChange,
+    prev,
+    next,
+    isStart,
+    isEnd,
+  } = useSwiperNav();
+
+  const handleOnSwiper = useCallback((swiper: SwiperType) => {
+    onSwiper(swiper);
+  }, [onSwiper]);
+
+  const handleOnChange = useCallback((swiper: SwiperType) => {
+    onSlideChange(swiper);
+  }, [onSlideChange]);
+
   return (
     <article className={styles.card}>
       <div className={styles.card__slide}>
-        <SwiperUI delay={5000} slides={gallery.map((image) => (
-          <ImageUI src={image} alt={image} key={image} aspectRatio={'2 / 1'} />))} />
+        <SwiperUI
+          delay={5000}
+          spaceBetween={0}
+          onSwiper={handleOnSwiper}
+          onSlideChange={handleOnChange}
+          slides={gallery.map((image) => (
+            <ImageUI src={image} alt={image} key={image} aspectRatio={'2 / 1'} />))
+          }
+        />
+        <nav className={styles.card__slide__nav}>
+          <Button disabled={isStart} onClick={prev} className={styles.button}>
+            <ChevronLeft size={16} />
+          </Button>
+          <Button disabled={isEnd} onClick={next} className={styles.button}>
+            <ChevronRight size={16} />
+          </Button>
+        </nav>
       </div>
       <div className={styles.card__inner}>
-        <h3>Villa standart oda</h3>
+        <h3 className={styles.title}>Villa standard oda</h3>
         <Separator />
+        <article className={'flex flex-row gap-2 items-center py-6'}>
+          <MartiniIcon size={24} />
+          <p className={styles.description}>Ульта все включено</p>
+        </article>
+        <div className={'flex flex-row justify-between py-6'}>
+          <ul className={styles.info}>
+            {FEATURES.map((feature) => {
+              const Icon = feature.Icon;
+              return (
+                <li className={styles.info__item} key={feature.label}>
+                  <p>{feature.label}</p>
+                  <Icon size={16} />
+                </li>
+              );
+            })}
+          </ul>
+          <div>
+            <p>1000 TL</p>
+          </div>
+        </div>
       </div>
     </article>
   );

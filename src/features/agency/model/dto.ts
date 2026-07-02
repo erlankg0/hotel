46b38d8d2b9dac67fe8722a07b5709be65073bd1
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2026-07-02 19:23:39 +0300

    Update dto.ts

--- src/features/agency/model/dto.ts
+++ src/features/agency/model/dto.ts
@@ -4,4 +4,5 @@ import { IsNotEmpty, fileSchema } from '@/shared/validator';
 
 const AgecySchema = z.object({
   title: z.string(IsNotEmpty).min(3, { message: 'Минимум 3 символа' }),
+  description: z.string(IsNotEmpty).min(10, { message: 'Минимум 10 символа' })
 });

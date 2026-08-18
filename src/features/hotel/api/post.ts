f815b8232dde7ea3dd0597d1d405bca253403864
erlankg0 <era.ab.02@gmail.com>
2026-08-18 12:02:19 +0300

    fix

--- src/features/hotel/api/post.ts
+++ src/features/hotel/api/post.ts
@@ -4,5 +4,5 @@ import type { BaseResponse } from '@/shared/types/response'
 import type { HotelType, HotelDto } from '../model/types'
 
 export const post = async (dto: HotelDto): Promise<AxiosResponse<BaseResponse<HotelType>>> => {
-    return await axiosInstance.post<BaseResponse<HotelType>>('hotel', dto)
+    return await axiosInstance.post<BaseResponse<HotelType>>('hotels', dto)
 }
